179fa9c4d7a1a00eb0818b2e56aeed3db8200612
Wilfor Ibanez <aln.wil4@outlook.es>
2023-06-06 12:01:52 -0400

    todoist path

--- Todoist-BDD/src/code/factoryBrowser/Chrome.cs
+++ Todoist-BDD/src/code/factoryBrowser/Chrome.cs
@@ -12,7 +12,7 @@ namespace Todoist_BDD.src.code.factoryBrowser
     {
         public IWebDriver Create()
         {
-            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
             string chromeDriverPath = $"{path}/src/resources/driver";
             //string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
             var chromeOptions = new ChromeOptions();
